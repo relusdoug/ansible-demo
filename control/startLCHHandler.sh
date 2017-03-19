b8b9d2b6ef9d02257e8b3c8574d42029aef3b3cb
Douglas Yoon <doug@reluscloud.com>
2017-03-19 15:29:04 +0000

    FEATURE: resize autoscaling group, once everything is up and running

--- control/startLCHHandler.sh
+++ control/startLCHHandler.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
+
+# set the desired instance number > 0
+#echo "App"
+aws autoscaling set-desired-capacity --desired-capacity 2 --auto-scaling-group-name \
+`aws autoscaling describe-auto-scaling-groups | \
+grep -e AutoScalingGroupName | \
+grep -e "DY-Ansible-Demo-AppStack-AppASG" | \
+sed -e s/'"AutoScalingGroupName":'// | \
+sed -e s/'[ \t\n\r,"]'//g `
+
+#echo "web"
+aws autoscaling set-desired-capacity --desired-capacity 2 --auto-scaling-group-name \
+`aws autoscaling describe-auto-scaling-groups | \
+grep -e AutoScalingGroupName | \
+grep -e "DY-Ansible-Demo-AppStack-WebASG" | \
+sed -e s/'"AutoScalingGroupName":'// | \
+sed -e s/'[ \t\n\r,"]'//g `
+
+# start monitoring the SQS queue for Lifecycle Hook events
 x=`grep QueueName createApplicationEnvironment.json | sed s/\"QueueName\":// | sed s/\"//g | sed s/\,// | sed s/\ //g`
 y=`aws sqs list-queues | grep $x`
 
-echo $x
-echo $y
+#echo $x
+#echo $y
 eval node inventory.js $y 
 
